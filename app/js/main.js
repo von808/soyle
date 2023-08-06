f513f95faae773be2f42e47ff269eecf33d44245
Alexey Sergeev <sergeevalexeyweb@gmail.com>
2023-08-06 17:49:44 +0700

    adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,14 @@
+const burgerBtn = document.querySelector('.burger-btn');
+const burgerMenu = document.querySelector('.burger-menu');
 const help = document.querySelector('.cards-lesson__help');
 const play = document.querySelector('.cards-lesson__controls-play');
 const autoPlay = document.querySelector('.cards-lesson__content-autoplay');
 const random = document.querySelector('.cards-lesson__controls-random');
+
+burgerBtn.addEventListener('click', function (e) {
+  burgerBtn.classList.toggle('active');
+  burgerMenu.classList.toggle('active');
+});
 
 help.addEventListener('click', function (e) {
   help.classList.toggle('active');
